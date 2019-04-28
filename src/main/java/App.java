2515f13b29fe1bc1f122611286f6d10225ffbe04
jamesandere <jamesandere56@gmail.com>
2019-04-28 13:19:16 +0300

    Create route path for landing page

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,35 +1,35 @@
-// import java.util.Map;
-// import java.util.HashMap;
-// import java.util.List;
-// import java.util.ArrayList;
-// import spark.ModelAndView;
-// import spark.template.velocity.VelocityTemplateEngine;
-// import static spark.Spark.*;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
 
-// public class App {
-//     public static void main(String[] args) {
-//       ProcessBuilder process = new ProcessBuilder();
-//          Integer port;
-//          if (process.environment().get("PORT") != null) {
-//              port = Integer.parseInt(process.environment().get("PORT"));
-//          } else {
-//              port = 4567;
-//          }
+public class App {
+    public static void main(String[] args) {
+      ProcessBuilder process = new ProcessBuilder();
+         Integer port;
+         if (process.environment().get("PORT") != null) {
+             port = Integer.parseInt(process.environment().get("PORT"));
+         } else {
+             port = 4567;
+         }
     
-//         setPort(port);
+        setPort(port);
 
-//         staticFileLocation("/public");
-//         String layout = "templates/layout.vtl";
+        staticFileLocation("/public");
+        String layout = "templates/layout.vtl";
 
 
-//          get("/", (request, response) -> {
-//       Map<String, Object> model = new HashMap<String, Object>();
-//       model.put("animals", Animal.all());
-//       model.put("endangeredAnimals", Endangered.all());
-//       model.put("sightings", Sighting.all());
-//       model.put("template", "templates/index.vtl");
-//       return new ModelAndView(model, layout);
-//     }, new VelocityTemplateEngine());
+         get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("animals", Animal.all());
+      model.put("endangeredAnimals", Endangered.all());
+      model.put("sightings", Sighting.all());
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
 
 
 //          get("/sightings", (request, response) -> {
